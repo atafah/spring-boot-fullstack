d0f36547562d42ae88311bc63c1ef56e4ad69250
anas-1337 <ProfessionalProgrammer1337@gmail.com>
2025-08-23 16:57:58 +0100

    spring initializr

---
+++ src/test/java/com/anastafah/SpringBootFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anastafah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
